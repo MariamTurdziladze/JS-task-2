3b6ed652aca1213a7871aeed7a2dd8225699d56e
MariamTurdziladze <mariamt789@gmail.com>
2024-11-04 22:38:09 +0400

    done

---
+++ script.js
@@ -0,0 +1,30 @@
+let a = 20;
+let b = a * a;
+
+console.log(b);
+
+if (b > 100) {
+    console.log("The number " + b + " is more than 100");
+} else {
+    console.log("The number " + b + " is less than 100");
+}
+
+
+
+let c = 50;
+
+if (c > 50 && c < 100) {
+    console.log("The number " + c + " is in range of 50-100");
+} else {
+    console.log("The number " + c + " isn't in range of 50-100");
+}
+
+
+
+let x = 0;
+
+if (x <= 0 ) {
+    console.log("The number " + x + " is negative or zero");
+} else {
+    console.log("The number " + x + " isn't negative or zero");
+}
